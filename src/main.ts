079af38c5e441ad493f82aa386aef0efdce09a9a
Jose Santos <josejaviersantossantana@gmail.com>
2026-03-11 19:40:18 -0300

    Finalizar Proyecto Base

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { log } from 'console';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  log(`Server running on port ${process.env.PORT ?? 4001}`);
 }
 bootstrap();
